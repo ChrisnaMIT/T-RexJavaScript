b6428fc05e7702a36b071cf5c34437a142a5d89f
chrisna <chrisna.mitt@gmail.com>
2024-11-30 17:40:12 +0100

    debut t-rex

---
+++ js/main.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dino = document.querySelector('.dino')
+    const grid = document.querySelector('.grid')
+    let gravity = 0.9
+
+    function control(e) {
+        if (e.code=== "Space"){
+            console.log('jump')
+            jump()
+        }
+    }
+
+    let position = 0
+    function jump (){
+        let count = 0
+        let timerId = setInterval(function () {
+            // permet au dino de sauter en haut
+            position += 30
+            position = position * gravity
+            dino.style.bottom = position + 'px'
+
+            //permet au dino de redescendre
+
+
+        }, 20)
+    }
+
+
+
+
+    document.addEventListener('keydown', control)
+
+
+})
